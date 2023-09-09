7c300f11a1db8908bc834168d36cce46000c75df
yearachoi <22200750@handong.ac.kr>
2023-09-09 14:54:44 +0900

    Update addWord(4) Program

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -1,11 +1,35 @@
 package com.mycom.word;
 
-public class WordCRUD implements ICRUD{ // ICRUD를 구현한 구현체
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class WordCRUD implements ICRUD{ // ICRUD를 구현한 구현체
+    // 배열 리스트를 사용하여 동적 데이터를 관리할 수 있도록 ArrayList 선언
+    ArrayList<Word> list;
+    Scanner s; // 사용자에게 입력받아올 수 있도록 스캐너 정의
+
+    WordCRUD(Scanner s){
+        list = new ArrayList<>();
+        this.s =s;
+    }
     // 각각의 함수가 어떻게 기능할지 구현
-    @Override
+    @Override // 사용자에게 데이터를 입력받는 부분
     public Object add() {
-        return null;
+        System.out.println("=> 난이도(1,2,3) & 새 단어 입력 : ");
+        int level = s.nextInt();
+        String word = s.nextLine();
+        // 1 driveway
+        System.out.println("뜻 입력 : ");
+        String meaning = s.nextLine();
+        // 차고 진입로
+        return new Word(0, level, word, meaning);
+    }
+
+    // 입력받은 데이터를 리스트에 추가하는 부분
+    public void addItem(){ // WordManager에서 호출할 함수
+        Word one = (Word)add();
+        list.add(one);
+        System.out.println("새 단어가 단어장에 추가되었습니다. ");
     }
 
     @Override
--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -1,8 +1,44 @@
 package com.mycom.word;
 
-public class WordManager { // WordCRUD를 이용해 CRUD 기능을 구현
+import java.sql.SQLOutput;
+import java.util.Scanner; // 스케너 함수 임포트
 
-    public void start() {
-        System.out.println("WordManager!!!"); // WordManager에서 메인 역할을 하는 start함수
+public class WordManager { // WordCRUD를 이용해 CRUD 기능을 구현
+    Scanner s = new Scanner(System.in); // 멤버변수로 스캐너 선언
+    WordCRUD wordCRUD;
+
+    WordManager() {
+        wordCRUD = new WordCRUD(s);
+    }
+
+    public int selectMenu() {
+        System.out.print("*** 영단어 마스터 ***\n"
+                + "********************\n"
+                + "1. 모든 단어 보기\n"
+                + "2. 수준별 단어 보기\n"
+                + "3. 단어 검색\n"
+                + "4. 단어 추가\n"
+                + "5. 단어 수정\n"
+                + "6. 단어 삭제\n"
+                + "7. 파일 저장\n"
+                + "0. 나가기\n"
+                + "********************\n"
+                + "=> 원하는 메뉴는? ");
+        return s.nextInt(); // 사용자에게서 원하는 메뉴 받아와서 리턴
+    }
+
+    public void start() { // WordManager에서 메인 역할을 하는 start함수
+
+
+        while (true) {
+            int menu = selectMenu(); // 리턴된 값을 그대로 출력
+            if (menu == 0) break;
+            if (menu == 4) {
+                // 데이터를 추가하는 구문
+                wordCRUD.addItem();
+            } else if (menu == 1) {
+                // 리스트를 보여주는 구문
+            }
+        }
     }
 }
