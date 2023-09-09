05bedcc8a5549390d3856d11e0e421c2d31b3eb7
yearachoi <22200750@handong.ac.kr>
2023-09-09 14:27:23 +0900

    Create WordCRUD.java class

---
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -0,0 +1,25 @@
+package com.mycom.word;
+
+public class WordCRUD implements ICRUD{ // ICRUD를 구현한 구현체
+
+    // 각각의 함수가 어떻게 기능할지 구현
+    @Override
+    public Object add() {
+        return null;
+    }
+
+    @Override
+    public int update(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public int delete(Object obj) {
+        return 0;
+    }
+
+    @Override
+    public void selectOne(int id) {
+
+    }
+}
