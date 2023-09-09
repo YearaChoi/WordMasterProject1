8725b427f462e3642f81efc3ddf8df6ba20185f6
yearachoi <22200750@handong.ac.kr>
2023-09-09 14:27:49 +0900

    Create WordManager.java class

--- src/main/java/com/mycom/word/WordManager.java
+++ src/main/java/com/mycom/word/WordManager.java
@@ -1,4 +1,8 @@
 package com.mycom.word;
 
-public class WordManager {
+public class WordManager { // WordCRUD를 이용해 CRUD 기능을 구현
+
+    public void start() {
+        System.out.println("WordManager!!!"); // WordManager에서 메인 역할을 하는 start함수
+    }
 }
