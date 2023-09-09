74cbb2d72f622b6e0ed4ac408e2c34ef12e250fa
yearachoi <22200750@handong.ac.kr>
2023-09-09 14:28:17 +0900

    Update Main.java

--- src/main/java/com/mycom/word/Main.java
+++ src/main/java/com/mycom/word/Main.java
@@ -2,7 +2,7 @@ package com.mycom.word;
 
 public class Main {
 
-    public static void main(String[] args) {
-        //new WordManager().start;
+    public static void main(String[] args) { // WordManager을 이용하여 프로그램을 실제 구동
+        new WordManager().start(); // WordManager에서 start함수 호출
     }
 }
